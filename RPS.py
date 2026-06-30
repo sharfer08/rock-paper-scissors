c588d3573968962d5bfc5237a039e91f60d7600e
sharfer08 <shareenfernandes08@gmail.com>
2026-06-30 11:22:54 +0530

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,18 +1,33 @@
-def player(prev_play, opponent_history=[]):
+def player(prev_play, opponent_history=[], play_order={}):
+    # 1. Reset memory at the start of a brand new match
     if not prev_play:
         opponent_history.clear()
+        play_order.clear()
         return "R"
 
+    # 2. Add the opponent's last move to our running history list
     opponent_history.append(prev_play)
-    counter = {'R': 'P', 'P': 'S', 'S': 'R'}
-    first_moves = "".join(opponent_history[:4])
+    guess = "R"  # Default safe guess
 
-    if first_moves.startswith("RR"):
-        quincy_pattern = ["R", "R", "P", "P", "S"]
-        current_round = len(opponent_history)
-        quincy_move = quincy_pattern[current_round % 5]
-        return counter[quincy_move]
-    elif first_moves == "RPSR":
-        return counter[prev_play]
-    else:
-        return counter[prev_play]
+    # 3. Pattern Matching Logic
+    n = 3  # Look back at sequences of 3 moves to predict the 4th
+    if len(opponent_history) >= n:
+        last_three = "".join(opponent_history[-n:])
+
+        # Record the 4-move combination that just happened into our dictionary
+        if len(opponent_history) > n:
+            past_pattern = "".join(opponent_history[-(n+1):])
+            play_order[past_pattern] = play_order.get(past_pattern, 0) + 1
+
+        # Check history to see what they usually play after this specific sequence
+        potential_next = [last_three + "R", last_three + "P", last_three + "S"]
+        sub_counts = {move[-1]: play_order.get(move, 0) for move in potential_next}
+        
+        # Predict their next move based on the highest count
+        prediction = max(sub_counts, key=sub_counts.get)
+        
+        # Counter their predicted move to win the round
+        ideal_response = {'R': 'P', 'P': 'S', 'S': 'R'}
+        guess = ideal_response[prediction]
+
+    return guess
