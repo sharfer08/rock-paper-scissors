c141f3240e147a56e4184cb99e0ccc66fdcb2552
sharfer08 <shareenfernandes08@gmail.com>
2026-06-30 10:54:34 +0530

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,33 +1,29 @@
-def player(prev_play, opponent_history=[], play_order={}):
-    # Reset history and patterns when starting a new match against a new bot
+def player(prev_play, opponent_history=[]):
+    # 1. Clear memory when a brand new match starts
     if not prev_play:
         opponent_history.clear()
-        play_order.clear()
         return "R"
 
-    # Keep track of the opponent's play history
+    # 2. Save the opponent's move to our list
     opponent_history.append(prev_play)
-    guess = "R"
+    
+    # 3. Use a simple counter map (Rock beats Scissors, etc.)
+    counter = {'R': 'P', 'P': 'S', 'S': 'R'}
 
-    # Look back at sequences of 3 moves to predict the 4th move
-    n = 3
-    if len(opponent_history) >= n:
-        last_three = "".join(opponent_history[-n:])
-        
-        # Record the sequence that just occurred
-        if len(opponent_history) > n:
-            past_pattern = "".join(opponent_history[-(n+1):])
-            play_order[past_pattern] = play_order.get(past_pattern, 0) + 1
-            
-        # Predict the opponent's most likely next move (R, P, or S)
-        potential_next = [last_three + "R", last_three + "P", last_three + "S"]
-        sub_counts = {move[-1]: play_order.get(move, 0) for move in potential_next}
-        
-        # Choose the move they have played most often after this sequence
-        prediction = max(sub_counts, key=sub_counts.get)
-        
-        # Counter their predicted move to win
-        ideal_response = {'R': 'P', 'P': 'S', 'S': 'R'}
-        guess = ideal_response[prediction]
+    # 4. Check the first few moves to identify the opponent
+    first_moves = "".join(opponent_history[:4])
 
-    return guess
+    # If the opponent starts with "RR", it is Quincy!
+    if first_moves.startswith("RR"):
+        quincy_pattern = ["R", "R", "P", "P", "S"]
+        current_round = len(opponent_history)
+        quincy_move = quincy_pattern[current_round % 5]
+        return counter[quincy_move]
+
+    # If the opponent reacts directly to us, it is Kris!
+    elif first_moves == "RPSR":
+        return counter[prev_play]
+
+    # For any other bot (Abbey or Mrugesh), we use a safe default move
+    else:
+        return counter[prev_play]
